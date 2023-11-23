cd02376cc1c606c2ccec2db6874c8d3cd8bb2eaa
Christopher Riquelme <criquelmes@live.com>
2023-11-23 01:19:57 -0300

    Sección 14: Creación de rutas de la aplicación

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Order #abc</h1>
+    </div>
+  );
+}
---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Listado de ordenes</h1>
+    </div>
+  );
+}
