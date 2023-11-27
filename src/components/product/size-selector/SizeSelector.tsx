343ac001af150ddddb0bb0d51eebad434a61595e
Christopher Riquelme <criquelmes@live.com>
2023-11-27 03:54:06 -0300

    Sección 15: Slideshow Desktop &  Mobile

---
+++ src/components/product/size-selector/SizeSelector.tsx
@@ -0,0 +1,28 @@
+import type { Size } from "@/interfaces";
+import clsx from "clsx";
+
+interface Props {
+  selectedSize: Size;
+  avaliableSizes: Size[];
+}
+
+export const SizeSelector = ({ selectedSize, avaliableSizes }: Props) => {
+  return (
+    <div className="my-5">
+      <h3 className="font-bold mb-4">Tallas disponibles</h3>
+
+      <div className="flex">
+        {avaliableSizes.map((size) => (
+          <button
+            key={size}
+            className={clsx("mx-2 hover:underline text-lg", {
+              underline: size === selectedSize,
+            })}
+          >
+            {size}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
