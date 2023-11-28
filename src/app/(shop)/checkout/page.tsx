edb513fd4a811894465729d670ff58b67ac67899
Christopher Riquelme <criquelmes@live.com>
2023-11-28 11:26:08 -0300

    Sección 15: Pantalla de Orden de pago

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,107 @@
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
 export default function Home() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px]">
+        <Title title="Verificar orden" />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <span className="text-xl">Añadir otro item</span>
+            <Link href="/cart" className="underline mb-5">
+              Editar carrito
+            </Link>
+
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  alt={product.title}
+                  className="mr-5 rounded"
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x3</p>
+                  <p className="font-semibold">
+                    Subtotal: ${product.price * 3}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Checkout - Resumen de orden*/}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl font-bold mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p className="text-xl">Christopher Riquelme</p>
+              <p>Burgos 400</p>
+              <p>Rancagua</p>
+              <p>Región del Libertador Bernardo Ohiggins</p>
+              <p>Código Postal: 258245</p>
+              <p>+569 58997922</p>
+            </div>
+
+            {/* Divider */}
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+
+            <h2 className="text-2xl font-bold mb-2">Resumen de orden</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">3 artículos</span>
+
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="text-2xl mt-5">Total:</span>
+              <span className="text-right text-2xl mt-5">$ 100</span>
+            </div>
+            <div className="mt-5 mb-2 w-full">
+              <p className="mt-5">
+                {/* Disclaimer */}
+                <span className="text-xs">
+                  Al hacer click en &quot;Colocar orden&quot;, aceptas nuestros{" "}
+                  <a href="#" className="underline">
+                    términos y condiciones
+                  </a>{" "}
+                  y{" "}
+                  <a href="#" className="underline">
+                    políticas de privacidad.
+                  </a>
+                </span>
+              </p>
+              <Link
+                className="btn-primary flex justify-center"
+                href="/orders/123"
+              >
+                {" "}
+                Colocar Orden
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
