d911afd0d9e45d53c75ae758975c3fa0b886bcb7
Christopher Riquelme <criquelmes@live.com>
2023-12-10 01:28:31 -0300

    Fix condición eejcución de seed en producción

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,5 +1,5 @@
-import { initialData } from "./seed";
 import prisma from "../lib/prisma";
+import { initialData } from "./seed";
 
 async function main() {
   // 1. Borrar registros previos
@@ -9,10 +9,23 @@ async function main() {
     prisma.category.deleteMany(),
   ]);
 
+  // 2. Categorias
+
+  const { categories, products } = initialData;
+
+  const categoriesData = categories.map((category) => ({
+    name: category,
+  }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  console.log(categoriesData);
   console.log("Seed ejecutado correctamente");
 }
 
 (() => {
-  if (process.env.NODE_ENV !== "production") return;
+  if (process.env.NODE_ENV === "production") return;
   main();
 })();
