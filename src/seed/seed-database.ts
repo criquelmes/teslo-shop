abb67c6180a0da238963579259c0aacb7350ed04
Christopher Riquelme <criquelmes@live.com>
2023-12-11 01:26:18 -0300

    Fin Sección 16: Preparación de base de datos. Seed.

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -3,11 +3,11 @@ import { initialData } from "./seed";
 
 async function main() {
   // 1. Borrar registros previos
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ]);
+  //   await Promise.all([
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  //   ]);
 
   // 2. Categorias
 
@@ -21,7 +21,35 @@ async function main() {
     data: categoriesData,
   });
 
-  console.log(categoriesData);
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  // 3. Productos
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // Images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
   console.log("Seed ejecutado correctamente");
 }
 
