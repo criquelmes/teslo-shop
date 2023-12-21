3f325f57380b772cdce625c9d33ae7a308841bd4
Christopher Riquelme <criquelmes@live.com>
2023-12-21 01:25:22 -0300

    Sección 17: Inicio paginación

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,8 +1,22 @@
+import { redirect } from "next/navigation";
 import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 
-export default async function Home() {
-  const { products } = await getPaginatedProductsWithImages();
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const { products } = await getPaginatedProductsWithImages({ page });
+
+  if (products.length === 0) {
+    redirect("/");
+  }
+
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
